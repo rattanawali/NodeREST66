507e1b3e4ae938bcf9c9ad60104607bef70c96b7
rattanawali <tayatt1145@gmail.com>
2024-02-05 09:40:07 +0700

    ...

--- src/SequlizeSQLiteCRUDBook.js
+++ src/SequlizeSQLiteCRUDBook.js
@@ -103,5 +103,5 @@ app.delete('/books/:id', (req, res) => {
 });
 
 // start the server
-const post = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
