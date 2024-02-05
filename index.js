c8e8a1ff9bf777b9e22109d9877c21c450f0a58b
rattanawali <tayatt1145@gmail.com>
2024-02-05 11:01:56 +0700

    ...

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
 // Description: Node Express REST API with Sequelize and SQLite CRUD Book
 // npm install express sequelize sqlite3
-// Run this file with node SequlizeSQLiteCRUDBook.js
+// Run this file withh node SeqlizeSQLiteCRUDBook.js
 // Test with Postman
 
-const express = require('express');
-const Sequelize = require('sequelize');
+const express = require("express");
+const Sequelize = require("sequelize");
 const app = express();
 
-// parse incoming requets
+// parse incoming requests
 app.use(express.json());
 
 // create a connection to the database
-const sequelize = new Sequelize('datebase', 'username', 'password', {
-    host: 'localhost',
-    dialect :'sqlite',
-    storage : '.Database/SQBook.sqlite'
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "./Database/SQBooks.sqlite",
 });
 
 // define the Book model
-const Book = sequelize.define('book', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    author: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
+const Book = sequelize.define("book", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
 });
 
 // create the books table if it doesn't exist
 sequelize.sync();
 
 // route to get all books
-app.get('/books', (req, res) => {
-    Book.findAll().then(books => {
-        res.json(books);
-    }).catch(err => {
-        res.status(500).send(err);
+app.get("/books", (req, res) => {
+  Book.findAll().then((books) => {
+      res.json(books);
+    }).catch((err) => {
+      res.status(500).send(err);
     });
 });
 
-// route to get a book by id 
-app.get('/books/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        if (!book) {
-            res.status(404).send('Book not found');
-        } else {
-            res.json(book);
-        }
-    }).catch(err => {
-        res.status(500).send(err);
+// route to get a book by id
+app.get("/books/:id", (req, res) => {
+  Book.findByPk(req.params.id).then((book) => {
+      if (!book) {
+        res.status(404).send("Book not found");
+      } else {
+        res.json(book);
+      }
+    }).catch((err) => {
+      res.status(500).send(err);
     });
 });
 
 // route to create a new book
-app.post('/books', (req, res) => {
-    Book.create(req.body).then(book => {
-        res.send(book);
-    }).catch(err => {
-        res.status(500).send(err);
+app.post("/books", (req, res) => {
+  Book.create(req.body).then((book) => {
+      res.send(book);
+    }).catch((err) => {
+      res.status(500).send(err);
     });
 });
 
 // route to update a book
-app.put('/books/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        if (!book) {
-            res.status(404).send('Book not found');
-        } else {
-            book.update(req.body).then(() => {
-                res.send(book);
-            }).catch(err => {
-                res.status(500).send(err);
-            });
-        }
-    }).catch(err => {
-        res.status(500).send(err);
+app.put("/books/:id", (req, res) => {
+  Book.findByPk(req.params.id).then((book) => {
+      if (!book) {
+        res.status(404).send("Book not found");
+      } else {
+        book.update(req.body).then(() => {
+            res.send(book);
+          }).catch((err) => {
+            res.status(500).send(err);
+          });
+      }
+    }).catch((err) => {
+      res.status(500).send(err);
     });
 });
 
 // route to delete a book
-app.delete('/books/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        if (!book) {
-            res.status(404).send('Book not found');
-        } else {
-            book.destroy().then(() => {
-                res.send({});
-            }).catch(err => {
-                res.status(500).send(err);
-            });
-        }
-}).catch(err => {
-    res.status(500).send(err);
-});
+app.delete("/books/:id", (req, res) => {
+  Book.findByPk(req.params.id).then((book) => {
+      if (!book) {
+        res.status(404).send("Book not found");
+      } else {
+        book.destroy().then(() => {
+            res.send({});
+          }).catch((err) => {
+            res.status(500).send(err);
+          });
+      }
+    }).catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
 // start the server
